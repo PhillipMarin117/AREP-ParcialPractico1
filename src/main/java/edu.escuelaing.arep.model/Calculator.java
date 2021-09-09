86e5fc14358c486ee28fc68332c8b5777f6bdf0b
Nickolas Marin <nickolas.marin@mail.escuelaing.edu.co>
2021-09-09 10:09:31 -0500

    Primer commit

---
+++ src/main/java/edu.escuelaing.arep.model/Calculator.java
@@ -0,0 +1,19 @@
+package edu.escuelaing.arep.model;
+
+public class Calculator {
+
+    private Calculator Math;
+
+    public double sin(double angulo){
+        return Math.sin(angulo);
+    }
+
+    public double cos(double angulo){
+        return Math.cos(angulo);
+    }
+
+    public double tan(double angulo){
+        return Math.tan(angulo);
+    }
+
+}
